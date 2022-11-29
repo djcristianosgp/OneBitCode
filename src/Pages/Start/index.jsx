2b6e66aa497f7b95b61281aa1b1ac74dbe0067ee
djcristianosgp <djcristiano.sgp@hotmail.com>
2022-11-29 13:46:03 -0300

    Alteracao da Estilização

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { View, Text, Image, ScrollView } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 
 export default function Start() {
   return (
-    <View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
+    <View style={styles.container}>
+      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+        <View style={{ alignItems: "center" }}>
           <Image
-            source={require("../../assets/icons/logo3.pn3")}
+            source={require("../../assets/icons/logo3.png")}
+            style={styles.logo}
           />
-          <Text>
+          <Text style={styles.description}>
             Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"} o melhor nível.
           </Text>
         </View>
@@ -17,3 +18,22 @@ export default function Start() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  description: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  },
+});
